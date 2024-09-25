0bdecfe4311510688aa9edbdd8d8d2bd8c2ce285
nac-39 <83295760+nac-39@users.noreply.github.com>
2024-09-25 09:48:52 +0900

    [wezterm] タブに関する設定

--- .wezterm.lua
+++ .wezterm.lua
@@ -12,6 +12,22 @@ config.font_size = 17
 config.freetype_load_target = "Light"
 wezterm.font("UDEV Gothic NF", { weight = "Bold", stretch = "Normal", style = "Normal" }) -- /Users/nanaco.s/Library/Fonts/UDEVGothicNF-Regular.ttf, CoreText
 config.window_background_opacity = 0.8
+-- ぼかす
+config.macos_window_background_blur = 20
+
+-- タイトルバーを消す
+config.window_decorations = "RESIZE"
+
+-- タブが一つしかない時はタブを表示しない
+config.hide_tab_bar_if_only_one_tab = true
+
+config.window_frame = {
+  inactive_titlebar_bg = "none",
+  active_titlebar_bg = "none",
+}
+config.window_background_gradient = {
+  colors = { "#000000" },
+}
 
 --shortcut key
 
